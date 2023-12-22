50ed9df89ba672e8d96ab0d766af5c140a2246a7
DESKTOP-4L7GJRP\Orax <saad.oraxtech@gmail.com>
2023-12-22 13:17:16 +0500

    Status Bar Issue Resolved

--- app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/com/example/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,23 @@
 package com.example.happyplaces
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityAddHappyPlaceBinding
+import com.example.happyplaces.databinding.ActivityMainBinding
 
 class AddHappyPlaceActivity : AppCompatActivity() {
+    private var binding: ActivityAddHappyPlaceBinding?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_happy_place)
+        binding = ActivityAddHappyPlaceBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        setSupportActionBar(binding?.toolbarAddPlace)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        binding?.toolbarAddPlace?.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
     }
 }
